995ec177bc2c4c61c2d2ef93eb09d15d5949c508
Michael George <michaelgeorge180@gmail.com>
2015-05-23 17:11:46 -0700

    Spool now carries physical properties for calc, slip conditions are still broken :(

--- SpoolDemo1.py
+++ SpoolDemo1.py
@@ -15,29 +15,43 @@ class Table:
 
 
 class Spool:
-    def __init__(self, x, y, angle, minor, major):
+    def __init__(self, x, y, minor, major, mass):
         self.x = x
         self.y = y
         self.initialx = x
         self.initialy = y
-        self.angle = angle
+        self.angle = 0
         self.minor = minor
         self.major = major
+        self.vel = 0
+        self.angvel = 0
+        self.mass = mass
+        self.scalefactor = 10
+        self.paused = True
+        self.initialstringlength = 5*self.minor*2*PI
+        self.stringlength = self.initialstringlength
 
     def draw(self):
+        drawmajor = self.major*self.scalefactor
+        drawminor = self.minor*self.scalefactor
         fill(255)
         pushMatrix()
         translate(self.x, self.y)
         rotate(self.angle)
-        ellipse(0, 0, self.major*2, self.major*2)
-        ellipse(0, 0, self.minor*2, self.minor*2)
-        ellipse(self.major, 0, 10, 10)
-        ellipse(-1*self.major, 0, 10, 10)
+        if self.minor>self.major:
+            ellipse(0, 0, drawminor*2, drawminor*2)
+            ellipse(0, 0, drawmajor*2, drawmajor*2)
+        else:
+            ellipse(0, 0, drawmajor*2, drawmajor*2)
+            ellipse(0, 0, drawminor*2, drawminor*2)
+
+        ellipse(drawmajor, 0, 10, 10)
+        ellipse(-1*drawmajor, 0, 10, 10)
 
         popMatrix()
         pushMatrix()
-        translate(self.x, self.y+self.minor)
-        line(0, 0, 500, 0)
+        translate(self.x, self.y+drawminor)
+        line(0, 0, self.stringlength*self.scalefactor, 0)
         popMatrix()
 
     def setMinor(self, minor):
@@ -49,28 +63,44 @@ class Spool:
 
     def setOnTable(self, table):
         self.table = table
-        self.y = height-table.height-self.major
+        self.y = height-table.height-self.major*self.scalefactor
 
     def setAngle(self, angle):
         self.angle = angle
 
     def incrementAngle(self, increment):
-        self.angle = self.angle - increment
+        self.angle += increment
+        self.stringlength -= increment*self.minor
+        
+        if self.stringlength < 3:
+            self.paused = True
+
+        print self.stringlength
+            
+        
 
     def incrementPosition(self, x, y):
-        self.x += x
-        self.y += y
+        self.x += x*self.scalefactor
+        self.y += y*self.scalefactor
 
     def resetPosition(self):
         self.x = self.initialx
+        self.vel = 0
+        self.angvel = 0
+        self.stringlength = self.initialstringlength
+        self.angle = 0
+
+    def pause(self):
+        self.paused = not self.paused
 
 
-class ResetButton:
-    def __init__(self, x, y):
+class Button:
+    def __init__(self, x, y, text):
         self.x = x
         self.y = y
         self.width = 100
         self.height = 50
+        self.text = text
 
     def draw(self):
         pushMatrix()
@@ -79,7 +109,7 @@ class ResetButton:
         rect(0, 0, self.width, self.height)
         fill(0)
         textSize(20)
-        text("Reset", 11, 30)
+        text(self.text, 11, 30)
         popMatrix()
 
     def checkClicked(self, xpos, ypos):
@@ -91,22 +121,19 @@ def setup():
     frameRate(50)
     smooth()
     
-    global mass
-    mass = 5.0
     global major
     major = 6.0
     global minor
     minor = 2.0
     global force
-    force = 2.0
-    global scalefactor
-    scalefactor = 10
+    force = 5.0
 
     global resetButton
-    resetButton = ResetButton(height-50, width/2 - 50)
-
+    resetButton = Button(width/2 - 50, height-50, "Reset")
+    global pauseButton
+    pauseButton = Button(width/2 + 100, height-50, "   ||>")
     global spool 
-    spool = Spool(width/3, 0, 0, scalefactor*minor, scalefactor*major)
+    spool = Spool(width/3, 0, minor, major, 5.0)
     global table
     table = Table(200)
 
@@ -114,7 +141,6 @@ def setup():
 
 def draw():
     global force
-    global mass
     global minor
     global major
     global spool
@@ -124,6 +150,7 @@ def draw():
     table.draw()
 
     resetButton.draw()
+    pauseButton.draw()
     #print "Mass %s" % mass
     #print "major %s" % major
     #print "minor %s" % minor
@@ -140,82 +167,84 @@ def draw():
                 print "invalid input entered!"
         elif someinput[0:2] == "m=":
             try:
-                mass = float(someinput[2:])
+                spool.mass = float(someinput[2:])
                 print "set mass to %f" % mass
             except:
                 print "invalid input entered!"
         elif someinput[0:2] == "R=":
             try:
                 major = float(someinput[2:])
-                spool.setMajor(major*scalefactor)
+                spool.setMajor(major)
                 print "set major to %f" % major
             except:
                 print "invalid input entered!"
         elif someinput[0:2] == "r=":
             try:
                 minor = float(someinput[2:])
-                spool.setMinor(minor*scalefactor)
+                spool.setMinor(minor)
                 print "set minor to %f" % minor
             except:
                 print "invalid input entered!"
         elif someinput[0:2] == "rst":
             spool.resetPosition()
-
-    calculated = calc(mass, major, minor, force)
-    spool.incrementAngle(calculated[1])
-    spool.incrementPosition(calculated[0]*scalefactor, 0)
+    if not spool.paused:
+        calc(spool, major, minor, force)
 
 
 def mousePressed():
     if resetButton.checkClicked(mouse.x, mouse.y):
         spool.resetPosition()
+        spool.paused = True
+    if pauseButton.checkClicked(mouse.x, mouse.y):
+        spool.pause()
     
 
 
 
-def calc(m, R, r, P):
-    # p = input('Input Pulling Force: ')
-    #print "Force is %f" % P
+def calc(spool, major, minor, pull):
+
     # Calculate Mass Moment of Inertia
-    I = 1.0/2.0*(2.0*m/3.0*R**2.0+m/3.0*r**2.0)
-    #print "I is %f" % I
+    inertia = 1.0/2.0*(2.0*spool.mass/3.0*major**2.0+spool.mass/3.0*minor**2.0)
+    #print "inertia is %f" % inertia
     
-    # Force Matrix for Plot
-    #P = np.arange(0,3.1,.1)
     
     # Force Friction
-    #Ff = P - m*(-P*(R*(r-R)/(I-R**2.0)))
-    Ff = -1.0*P*(r+(I/(R*m)))/(R+(I/(R*m)))
-    #print "Ff is %f" % Ff
+    friction = -1.0*pull*(minor+(inertia/(major*spool.mass)))/(major+(inertia/(major*spool.mass)))
+    #print "friction is %f" % friction
     
     # Slip Condition
     mew = .19
     mewk = .95*mew
-    Ffmax = -m*9.81*mew
-    #print "Ffmax is %f" % Ffmax    
+    frictionmax = -spool.mass*9.81*mew
+    #print "frictionmax is %f" % frictionmax    
     
     # Test if Spool is slipping
-    #for i in range(0,Ff.size):  
     
-    if max(Ff, -Ff) > max(Ffmax, -Ffmax):
-        Ff = -1.0*m*9.81*mewk  
-    #print "Ff: %f Ffmax %f" % (Ff, Ffmax)
+    if max(friction, -friction) > max(frictionmax, -frictionmax):
+        friction = -1.0*spool.mass*9.81*mewk  
+    #print "friction: %f frictionmax %f" % (friction, frictionmax)
     
     # Calculate accelerations
-    a = (P+Ff)/m
+    a = (pull+friction)/spool.mass
     #print "Acceleration is %f" % a
-    alpha = (R*Ff+r*P)/I
+    alpha = (major*friction+minor*pull)/inertia
 
     #print "alpha is %f" % alpha
-    #print('a = ', a)
+    #print 'a is %f', a
 
     # Calculate Position increments Using Basic Kinematics
-    dt = 1.0 # a time step is one frame
-    dx = 1.0/2.0*a*dt**2.0
-    dtheta = 1.0/2.0*alpha*dt**2.0
+    dt = 1.0/50 # a time step is one frame
+    dx = 1.0/2.0*a*dt**2.0+spool.vel*dt
+    dtheta = dx/major # 1.0/2.0*alpha*dt**2.0+spool.angvel*dt
 
     #print "x moved by: %f, angle moved by %f" % (dx, dtheta)
-    return [dx, dtheta]
+    
+    # Check if spool is paused
+    # Increment Spool Positions
+    spool.incrementPosition(dx, 0)
+    spool.incrementAngle(dtheta)
+    spool.vel += a*dt
+    spool.angvel += a*dt
 
 if __name__== "__main__":
     run()
